06137b051dfde20e3e102f5f648ffea044fbc587
Tanya Gray <tanya.gray@theodi.org>
2026-03-23 19:14:54 +0000

    update volunteer shapes

--- src/shacl-transform/transform.js
+++ src/shacl-transform/transform.js
@@ -1,23 +1,23 @@
 // enrich-shapes.js
 import fs from "fs";
 import { Parser, Writer, DataFactory } from "n3";
-import { pascalCase } from "change-case";
+import { pascalCase, camelCase } from "change-case";
 
-const { namedNode, literal, quad } = DataFactory;
+const { namedNode, literal } = DataFactory;
 
 // ------------------ CONFIG ------------------
-const INPUT = "../astrea/volunteer-shapes.ttl";  // Astrea SHACL output
+const INPUT = "../../data/astrea/volunteer-shapes.ttl";
 const OUTPUT = "output.ttl";
 
 const VOL_NS = "https://solidproject.org/shapes/volunteer#";
 
-// Metadata to attach to all NodeShapes
+// Metadata
 const CREATED_DATE = "2026-03-23";
 const TERM_STATUS = "testing";
 const SOURCE_URL = "https://api.volunteeringdata.io/schema.ttl";
 const DERIVED_FROM = SOURCE_URL;
 
-// Prefixes for output
+// Prefixes
 const PREFIXES = {
   "volunteer-shapes": VOL_NS,
   sh: "http://www.w3.org/ns/shacl#",
@@ -30,8 +30,6 @@ const PREFIXES = {
 };
 
 // ------------------ UTILS ------------------
-
-// Find first object of a given predicate for a subject
 function getObject(subject, predicate, quads) {
   const q = quads.find(
     (x) =>
@@ -41,14 +39,12 @@ function getObject(subject, predicate, quads) {
   return q ? q.object.value : null;
 }
 
-// Get all objects for a predicate (for altLabels or broader)
 function getObjects(subject, predicate, quads) {
   return quads
     .filter((x) => x.subject.equals(subject) && x.predicate.value === predicate)
     .map((x) => x.object.value);
 }
 
-// Check if a subject has a given rdf:type
 function isType(subject, type, quads) {
   return quads.some(
     (x) =>
@@ -84,25 +80,38 @@ const propMap = new Map();
 for (const ps of propertyShapes) {
   const label = getObject(ps, "http://www.w3.org/2000/01/rdf-schema#label", quads);
   if (!label) continue;
-  const name = pascalCase(label);
+
+  const name = camelCase(label); // Property names in camelCase
+  let codeIdentifier = name;
+
+  // Ensure valid JS identifier
+  if (["value", "type"].includes(codeIdentifier)) codeIdentifier += "Prop";
+  if (!/^[a-zA-Z_$][0-9a-zA-Z_$]*$/.test(codeIdentifier)) {
+    codeIdentifier = codeIdentifier.replace(/^[^a-zA-Z_$]+/, "").replace(/[^0-9a-zA-Z_$]/g, "");
+  }
+
   const iri = namedNode(VOL_NS + name);
-  propMap.set(ps.value, { iri, name });
+  propMap.set(ps.value, { iri, name, codeIdentifier });
 }
 
 const nodeMap = new Map();
 for (const ns of nodeShapes) {
   const label = getObject(ns, "http://www.w3.org/2000/01/rdf-schema#label", quads);
   if (!label) continue;
-  const name = pascalCase(label) + "Shape";
+
+  const name = pascalCase(label) + "Shape"; // NodeShape names in PascalCase
+  let codeIdentifier = pascalCase(label); // NodeShape codeIdentifier also PascalCase
+  if (["Value", "Type"].includes(codeIdentifier)) codeIdentifier += "Node";
+
   const iri = namedNode(VOL_NS + name);
-  nodeMap.set(ns.value, { iri, name });
+  nodeMap.set(ns.value, { iri, name, codeIdentifier });
 }
 
 // ------------------ WRITE TTL ------------------
 const writer = new Writer({ prefixes: PREFIXES });
 
-// Write PropertyShapes with metadata
-for (const [oldIRI, { iri, name }] of propMap) {
+// Write PropertyShapes
+for (const [oldIRI, { iri, name, codeIdentifier }] of propMap) {
   const subject = namedNode(oldIRI);
 
   writer.addQuad(iri, namedNode(PREFIXES.sh + "type"), namedNode(PREFIXES.sh + "PropertyShape"));
@@ -114,16 +123,13 @@ for (const [oldIRI, { iri, name }] of propMap) {
                       getObject(subject, PREFIXES.skos + "definition", quads);
   if (description) writer.addQuad(iri, namedNode(PREFIXES.sh + "description"), literal(description));
 
-  // AltLabels from SKOS
   const altLabels = getObjects(subject, PREFIXES.skos + "altLabel", quads);
   for (const alt of altLabels) {
     writer.addQuad(iri, namedNode(PREFIXES.sh + "alternativeName"), literal(alt));
   }
 
-  // sh:codeIdentifier
-  writer.addQuad(iri, namedNode(PREFIXES.sh + "codeIdentifier"), literal(name));
-  
-  // Copy other predicates (datatype, path, class, nodeKind) except sh:pattern
+  writer.addQuad(iri, namedNode(PREFIXES.sh + "codeIdentifier"), literal(codeIdentifier));
+
   for (const q of quads.filter((x) => x.subject.equals(subject))) {
     const p = q.predicate.value;
     if (
@@ -133,11 +139,11 @@ for (const [oldIRI, { iri, name }] of propMap) {
       p === PREFIXES.sh + "pattern"
     ) continue;
     writer.addQuad(iri, q.predicate, q.object);
-}
+  }
 }
 
-// Write NodeShapes with metadata
-for (const [oldIRI, { iri, name }] of nodeMap) {
+// Write NodeShapes
+for (const [oldIRI, { iri, name, codeIdentifier }] of nodeMap) {
   const subject = namedNode(oldIRI);
 
   writer.addQuad(iri, namedNode(PREFIXES.sh + "type"), namedNode(PREFIXES.sh + "NodeShape"));
@@ -149,25 +155,21 @@ for (const [oldIRI, { iri, name }] of nodeMap) {
                       getObject(subject, PREFIXES.skos + "definition", quads);
   if (description) writer.addQuad(iri, namedNode(PREFIXES.sh + "description"), literal(description));
 
-  // Optional metadata
   writer.addQuad(iri, namedNode(PREFIXES.dc + "created"), literal(CREATED_DATE, namedNode(PREFIXES.xsd + "date")));
   writer.addQuad(iri, namedNode(PREFIXES.vs + "term_status"), literal(TERM_STATUS));
   writer.addQuad(iri, namedNode(PREFIXES.dc + "source"), namedNode(SOURCE_URL));
   writer.addQuad(iri, namedNode(PREFIXES.prov + "wasDerivedFrom"), namedNode(DERIVED_FROM));
 
-  // Alternative names
   const altLabels = getObjects(subject, PREFIXES.skos + "altLabel", quads);
   for (const alt of altLabels) {
     writer.addQuad(iri, namedNode(PREFIXES.sh + "alternativeName"), literal(alt));
   }
 
-  // Optional broader relationships
   const broader = getObjects(subject, PREFIXES.skos + "broader", quads);
   for (const b of broader) {
     writer.addQuad(iri, namedNode(PREFIXES.skos + "broader"), namedNode(b));
   }
 
-  // Map sh:property references to new IRIs
   for (const q of quads.filter((x) => x.subject.equals(subject))) {
     if (q.predicate.value === PREFIXES.sh + "property" && propMap.has(q.object.value)) {
       writer.addQuad(iri, q.predicate, propMap.get(q.object.value).iri);
@@ -178,8 +180,7 @@ for (const [oldIRI, { iri, name }] of nodeMap) {
     }
   }
 
-  // sh:codeIdentifier
-  writer.addQuad(iri, namedNode(PREFIXES.sh + "codeIdentifier"), literal(name.replace("Shape", "")));
+  writer.addQuad(iri, namedNode(PREFIXES.sh + "codeIdentifier"), literal(codeIdentifier));
 }
 
 // ------------------ OUTPUT ------------------
@@ -187,8 +188,8 @@ writer.end((err, result) => {
   if (err) return console.error(err);
 
   const formatted = result
-    .replace(/^(@prefix [^>]+>.)^\n+/g, "$1") // remove extra blank lines after prefixes
-    .replace(/^(volunteer-shapes.*)$/gm, "\n$1"); // blank line before each shape
+    .replace(/^(@prefix [^>]+>.)^\n+/g, "$1")
+    .replace(/^(volunteer-shapes.*)$/gm, "\n$1");
 
   fs.writeFileSync(OUTPUT, formatted);
   console.log("Done! Output written to", OUTPUT);
