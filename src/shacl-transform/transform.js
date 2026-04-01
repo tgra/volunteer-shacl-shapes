f288f3f5c297e7736a58e512b5629c981d20d90f
Tanya Gray <tanya.gray@theodi.org>
2026-04-01 19:47:54 +0100

    use sh: metadata

--- src/shacl-transform/transform.js
+++ src/shacl-transform/transform.js
@@ -1,4 +1,3 @@
-// enrich-shapes.js
 import fs from "fs";
 import { Parser, Writer, DataFactory } from "n3";
 import { pascalCase, camelCase } from "change-case";
@@ -6,44 +5,25 @@ import { pascalCase, camelCase } from "change-case";
 const { namedNode, literal } = DataFactory;
 
 // ------------------ CONFIG ------------------
-const INPUT = "../../data/astrea/volunteer-shapes.ttl";
-const OUTPUT = "output.ttl";
+const INPUT = "./shacl-play-convert-output.ttl";
+const SKOS_INPUT = "../../volunteer-schema.ttl"; // SKOS file
+const OUTPUT = "volunteer.ttl";
 const NAME_PREFIX = "volunteer";
-
+const REMOVE_STRING = "-https___ns.volunteeringdata.io_";
 const VOL_NS = "https://solidproject.org/shapes/volunteer#";
 
 // Metadata
-const CREATED_DATE = "2026-03-23";
+const CREATED_DATE = "2026-04-01";
 const TERM_STATUS = "testing";
 const SOURCE_URL = "https://api.volunteeringdata.io/schema.ttl";
 const DERIVED_FROM = SOURCE_URL;
 
-// Prefixes
-const PREFIXES = {
-  "volunteer-shapes": VOL_NS,
-  sh: "http://www.w3.org/ns/shacl#",
-  rdfs: "http://www.w3.org/2000/01/rdf-schema#",
-  skos: "http://www.w3.org/2004/02/skos/core#",
-  vs: "http://www.w3.org/2006/VS/terms/",
-  dc: "http://purl.org/dc/terms/",
-  prov: "http://www.w3.org/ns/prov#",
-  xsd: "http://www.w3.org/2001/XMLSchema#",
-};
-
 // ------------------ UTILS ------------------
 function getObject(subject, predicate, quads) {
   const q = quads.find(
-    (x) =>
-      x.subject.equals(subject) &&
-      x.predicate.value === predicate
+    (x) => x.subject.equals(subject) && x.predicate.value === predicate
   );
   return q ? q.object.value : null;
-}
-
-function getObjects(subject, predicate, quads) {
-  return quads
-    .filter((x) => x.subject.equals(subject) && x.predicate.value === predicate)
-    .map((x) => x.object.value);
 }
 
 function isType(subject, type, quads) {
@@ -55,143 +35,177 @@ function isType(subject, type, quads) {
   );
 }
 
+function getLocalName(iri) {
+  if (!iri) return null;
+  return iri.split(/[#/:]/).pop();
+}
+
+function getSkosEntry(iri) {
+  if (!iri) return null;
+  return skosMap.get(iri) || null;
+}
+
+function cleanString(strEdit, removeString) {
+  if (!strEdit) return null;
+  return String(strEdit)
+    .replace(new RegExp(removeString, "g"), "_")
+    .replace(/\s+/g, "_");
+}
+
 // ------------------ LOAD TTL ------------------
 const ttl = fs.readFileSync(INPUT, "utf8");
 const parser = new Parser();
 const quads = parser.parse(ttl);
 
+// Collect prefixes from file
+let PREFIXES = parser._prefixes || {};
+PREFIXES["volunteer_shape"] = VOL_NS;
+PREFIXES.sh ??= "http://www.w3.org/ns/shacl#";
+PREFIXES.rdfs ??= "http://www.w3.org/2000/01/rdf-schema#";
+PREFIXES.skos ??= "http://www.w3.org/2004/02/skos/core#";
+PREFIXES.vs ??= "http://www.w3.org/2006/VS/terms/";
+PREFIXES.dc ??= "http://purl.org/dc/terms/";
+PREFIXES.prov ??= "http://www.w3.org/ns/prov#";
+PREFIXES.xsd ??= "http://www.w3.org/2001/XMLSchema#";
+
+// ------------------ LOAD SKOS ------------------
+const skosTtl = fs.readFileSync(SKOS_INPUT, "utf8");
+const skosParser = new Parser();
+const skosQuads = skosParser.parse(skosTtl);
+const skosMap = new Map();
+
+for (const q of skosQuads) {
+  const s = q.subject.value;
+  if (!skosMap.has(s)) {
+    skosMap.set(s, { prefLabel: null, label: null, definition: null, altLabels: [], broader: [] });
+  }
+  const entry = skosMap.get(s);
+  const p = q.predicate.value;
+
+  if (p === PREFIXES.rdfs + "label") entry.label = q.object.value;
+  else if (p === PREFIXES.skos + "prefLabel") entry.prefLabel = q.object.value;
+  else if (p === PREFIXES.skos + "definition") entry.definition = q.object.value;
+  else if (p === PREFIXES.skos + "altLabel") entry.altLabels.push(q.object.value);
+  else if (p === PREFIXES.skos + "broader") entry.broader.push(q.object.value);
+
+  if (!entry.label && entry.prefLabel) entry.label = entry.prefLabel;
+}
+
 // ------------------ COLLECT SHAPES ------------------
 const subjects = [...new Set(quads.map((q) => q.subject.value))];
-
 const nodeShapes = [];
 const propertyShapes = [];
 
 for (const s of subjects) {
   const subject = namedNode(s);
-  if (isType(subject, "http://www.w3.org/ns/shacl#NodeShape", quads)) {
-    nodeShapes.push(subject);
-  }
-  if (isType(subject, "http://www.w3.org/ns/shacl#PropertyShape", quads)) {
-    propertyShapes.push(subject);
-  }
+  if (isType(subject, PREFIXES.sh + "NodeShape", quads)) nodeShapes.push(subject);
+  if (isType(subject, PREFIXES.sh + "PropertyShape", quads)) propertyShapes.push(subject);
 }
 
 // ------------------ MAP IRIs ------------------
 const propMap = new Map();
 for (const ps of propertyShapes) {
-  const label = getObject(ps, "http://www.w3.org/2000/01/rdf-schema#label", quads);
-  if (!label) continue;
+  const path = getObject(ps, PREFIXES.sh + "path", quads);
+  const skos = path && getSkosEntry(path);
+  let label = getObject(ps, PREFIXES.rdfs + "label", quads) || skos?.label || skos?.prefLabel || getLocalName(ps.value);
+  label = cleanString(label, REMOVE_STRING).toLowerCase();
+  
 
-  const name = camelCase(label); // Property names in camelCase
+  const name = label.toLowerCase().replace(/\s+/g, "_");
+  console.log(name)
   let codeIdentifier = name;
-
-  // Ensure valid JS identifier
   if (["value", "type"].includes(codeIdentifier)) codeIdentifier += "Prop";
   if (!/^[a-zA-Z_$][0-9a-zA-Z_$]*$/.test(codeIdentifier)) {
     codeIdentifier = codeIdentifier.replace(/^[^a-zA-Z_$]+/, "").replace(/[^0-9a-zA-Z_$]/g, "");
   }
 
   const iri = namedNode(VOL_NS + name);
-  propMap.set(ps.value, { iri, name, codeIdentifier });
+  
+  propMap.set(ps.value, { iri, name, codeIdentifier, skos });
 }
+
 
 const nodeMap = new Map();
 for (const ns of nodeShapes) {
-  const label = getObject(ns, "http://www.w3.org/2000/01/rdf-schema#label", quads);
-  if (!label) continue;
+  const target = getObject(ns, PREFIXES.sh + "targetSubjectsOf", quads);
+  const skos = target && getSkosEntry(target);
+  let label = getObject(ns, PREFIXES.rdfs + "label", quads) || skos?.label || skos?.prefLabel || getLocalName(ns.value);
+  label = cleanString(label, REMOVE_STRING);
+  
 
-  const name = pascalCase(label) + "Shape"; // NodeShape names in PascalCase
-  let codeIdentifier = pascalCase(`${NAME_PREFIX} ${label}`); // NodeShape codeIdentifier also PascalCase
-  if (["Value", "Type"].includes(codeIdentifier)) codeIdentifier += "Node";
-
+  const name = pascalCase(label);
   const iri = namedNode(VOL_NS + name);
-  nodeMap.set(ns.value, { iri, name, codeIdentifier });
+  let codeIdentifier = pascalCase(`${NAME_PREFIX}${label}`);
+  if (["Value", "Type"].includes(codeIdentifier)) codeIdentifier = NAME_PREFIX + codeIdentifier;
+
+  nodeMap.set(ns.value, { iri, name, codeIdentifier, skos });
 }
 
 // ------------------ WRITE TTL ------------------
 const writer = new Writer({ prefixes: PREFIXES });
 
-// Write PropertyShapes
-for (const [oldIRI, { iri, name, codeIdentifier }] of propMap) {
-  const subject = namedNode(oldIRI);
+function writeShape(iri, quadsForShape, codeIdentifier) {
+  // Add type triple
+  writer.addQuad(iri, namedNode(PREFIXES.sh + "type"), quadsForShape.type);
 
-  writer.addQuad(iri, namedNode(PREFIXES.sh + "type"), namedNode(PREFIXES.sh + "PropertyShape"));
-
-  const label = getObject(subject, "http://www.w3.org/2000/01/rdf-schema#label", quads);
-  if (label) writer.addQuad(iri, namedNode(PREFIXES.rdfs + "label"), literal(label));
-
-  const description = getObject(subject, "http://www.w3.org/2000/01/rdf-schema#comment", quads) ||
-                      getObject(subject, PREFIXES.skos + "definition", quads);
-  if (description) writer.addQuad(iri, namedNode(PREFIXES.sh + "description"), literal(description));
-
-  const altLabels = getObjects(subject, PREFIXES.skos + "altLabel", quads);
-  for (const alt of altLabels) {
-    writer.addQuad(iri, namedNode(PREFIXES.sh + "alternativeName"), literal(alt));
+  // Label
+  if (quadsForShape.label) {
+    writer.addQuad(iri, namedNode(PREFIXES.sh + "name"), literal(cleanString(quadsForShape.label, REMOVE_STRING)));
   }
 
+  // Comment / definition
+  if (quadsForShape.definition) {
+    writer.addQuad(iri, namedNode(PREFIXES.sh + "description"), literal(quadsForShape.definition));
+  }
+
+  // Code identifier
   writer.addQuad(iri, namedNode(PREFIXES.sh + "codeIdentifier"), literal(codeIdentifier));
 
-  for (const q of quads.filter((x) => x.subject.equals(subject))) {
-    const p = q.predicate.value;
-    if (
-      p.endsWith("#label") ||
-      p.endsWith("definition") ||
-      p.endsWith("comment") ||
-      p === PREFIXES.sh + "pattern"
-    ) continue;
+  // Copy other triples
+  for (const q of quadsForShape.otherQuads) {
     writer.addQuad(iri, q.predicate, q.object);
   }
+
+  // Flush this shape so it ends with a period
+  writer._write("\n"); // <-- Add blank line after each shape
 }
 
-// Write NodeShapes
+// --- Write PropertyShapes ---
+for (const [oldIRI, { iri, name, codeIdentifier }] of propMap) {
+  const subject = namedNode(oldIRI);
+  const typeQuad = { type: namedNode(PREFIXES.sh + "PropertyShape") };
+  const label = getObject(subject, PREFIXES.rdfs + "label", quads) || getLocalName(subject.value);
+  const definition = null; // or get from SKOS
+  const otherQuads = quads.filter(x => x.subject.equals(subject));
+  writeShape(iri, { type: typeQuad.type, label, definition, otherQuads }, codeIdentifier);
+}
+
+// --- Write NodeShapes ---
 for (const [oldIRI, { iri, name, codeIdentifier }] of nodeMap) {
   const subject = namedNode(oldIRI);
+  const typeQuad = { type: namedNode(PREFIXES.sh + "NodeShape") };
+  const label = getObject(subject, PREFIXES.rdfs + "label", quads) || getLocalName(subject.value);
+  const definition = null; // or get from SKOS
 
-  writer.addQuad(iri, namedNode(PREFIXES.sh + "type"), namedNode(PREFIXES.sh + "NodeShape"));
+  // Map sh:property URLs to PropertyShape IRIs
+  const otherQuads = quads
+    .filter(x => x.subject.equals(subject))
+    .map(q => {
+      if (q.predicate.value === PREFIXES.sh + "property") {
+        const mappedProp = propMap.get(q.object.value);
+        if (mappedProp) {
+          return { predicate: q.predicate, object: mappedProp.iri };
+        }
+      }
+      return q;
+    });
 
-  const label = getObject(subject, "http://www.w3.org/2000/01/rdf-schema#label", quads);
-  if (label) writer.addQuad(iri, namedNode(PREFIXES.rdfs + "label"), literal(label));
-
-  const description = getObject(subject, "http://www.w3.org/2000/01/rdf-schema#comment", quads) ||
-                      getObject(subject, PREFIXES.skos + "definition", quads);
-  if (description) writer.addQuad(iri, namedNode(PREFIXES.sh + "description"), literal(description));
-
-  writer.addQuad(iri, namedNode(PREFIXES.dc + "created"), literal(CREATED_DATE, namedNode(PREFIXES.xsd + "date")));
-  writer.addQuad(iri, namedNode(PREFIXES.vs + "term_status"), literal(TERM_STATUS));
-  writer.addQuad(iri, namedNode(PREFIXES.dc + "source"), namedNode(SOURCE_URL));
-  writer.addQuad(iri, namedNode(PREFIXES.prov + "wasDerivedFrom"), namedNode(DERIVED_FROM));
-
-  const altLabels = getObjects(subject, PREFIXES.skos + "altLabel", quads);
-  for (const alt of altLabels) {
-    writer.addQuad(iri, namedNode(PREFIXES.sh + "alternativeName"), literal(alt));
-  }
-
-  const broader = getObjects(subject, PREFIXES.skos + "broader", quads);
-  for (const b of broader) {
-    writer.addQuad(iri, namedNode(PREFIXES.skos + "broader"), namedNode(b));
-  }
-
-  for (const q of quads.filter((x) => x.subject.equals(subject))) {
-    if (q.predicate.value === PREFIXES.sh + "property" && propMap.has(q.object.value)) {
-      writer.addQuad(iri, q.predicate, propMap.get(q.object.value).iri);
-    } else if (!q.predicate.value.endsWith("#label") &&
-               !q.predicate.value.endsWith("definition") &&
-               !q.predicate.value.endsWith("comment")) {
-      writer.addQuad(iri, q.predicate, q.object);
-    }
-  }
-
-  writer.addQuad(iri, namedNode(PREFIXES.sh + "codeIdentifier"), literal(codeIdentifier));
+  writeShape(iri, { type: typeQuad.type, label, definition, otherQuads }, codeIdentifier);
 }
 
 // ------------------ OUTPUT ------------------
 writer.end((err, result) => {
   if (err) return console.error(err);
-
-  const formatted = result
-    .replace(/^(@prefix [^>]+>.)^\n+/g, "$1")
-    .replace(/^(volunteer-shapes.*)$/gm, "\n$1");
-
-  fs.writeFileSync(OUTPUT, formatted);
+  fs.writeFileSync(OUTPUT, result);
   console.log("Done! Output written to", OUTPUT);
 });
